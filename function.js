f6aeb5238aa3c4647288201818ceeb83e55380ab
Jetdoc345 <jw600345@gmail.com>
2024-10-03 14:07:21 -0400

    Add files via upload

---
+++ function.js
@@ -0,0 +1,19 @@
+console.log("function");
+
+// convert celcius to farenheit project //
+
+        function converttemperature()
+        
+        {
+       
+        let Fahrenheit = (deg F);
+        let Celcius = (deg C);
+        const = (9/5 + 32);
+        let (deg Celcius * const = deg farenheit);
+        let (deg Celcius = a);
+        let (deg farenheit = b);
+        let (a* const = b)
+        
+        }
+
+
